ef0c77d33535300d09f34902313fa2f1be11b1c0
Laura Griffee <laura@mailzone.com>
2018-07-03 21:44:23 -0600

    feat: Implement and instantiate the Enemy

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -29,6 +32,9 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+var e1 = new Enemy(0, 0, 0);
+var allEnemies = [e1];
 
 
 
