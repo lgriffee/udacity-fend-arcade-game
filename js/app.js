aa1a99d7ba317d35a61cf8f217a9c21ddbe9eaac
Laura Griffee <laura@mailzone.com>
2018-07-03 22:12:18 -0600

    feat: Implement and instantiate the Player

--- js/app.js
+++ js/app.js
@@ -1,45 +1,90 @@
+/*
+*
+* ENEMY CLASS
+*
+*/
+
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
     this.speed = speed;
 };
 
-// Update the enemy's position, required method for game
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //TODO: Update enemy location
+    //TODO: Handle collision with the Player
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
+/*
+*
+* PLAYER CLASS
+*
+*/
+
+// The player the user controls
+var Player = function(x, y) {
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+};
+
+// Update the player's position
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    //TODO: Update player location
+    //TODO: Handle collision with Enemies
+};
+
+// Draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Handle keyboard input for the player
+Player.prototype.handleInput = function(key) {
+    //TODO: handle key input
+    //TODO: prevent player from moving off screen
+    //TODO: determine if player reaches water
+};
+
+// Reset the player
+Player.prototype.reset = function() {
+    //TODO: reset player if they reach the water
+};
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/*
+*
+* GAMEPLAY
+*
+*/
 
 var e1 = new Enemy(0, 0, 0);
 var allEnemies = [e1];
+var player = new Player(0, 0, 0);
 
 
+/*
+*
+* EVENT LISTENERS
+*
+*/
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
